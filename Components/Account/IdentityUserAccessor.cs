5679250357454643abda4943354b618e5bc8aedc
Fergal Moran <fergal.moran@wasptech.com>
2023-12-01 16:53:34 +0000

    Add project files.

---
+++ Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using api.fergl.ie.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace api.fergl.ie.Components.Account
+{
+  internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+  {
+    public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+    {
+      var user = await userManager.GetUserAsync(context.User);
+
+      if (user is null)
+      {
+        redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+      }
+
+      return user;
+    }
+  }
+}
